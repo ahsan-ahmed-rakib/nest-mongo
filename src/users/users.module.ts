f01db5e4628de7db666cf89c60d9fe4cf257a773
Ahsan Ahmed Rakib <ahsan.rakib@technonext.com>
2024-06-02 17:29:59 +0600

    One to One relationship

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/User.schema';
+import {
+  UserSettings,
+  UserSettingsSchema,
+} from 'src/schemas/UserSettings.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: UserSettings.name, schema: UserSettingsSchema },
+    ]),
   ],
   providers: [UsersService],
   controllers: [UsersController],
