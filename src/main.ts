d8308ed7730d3f2c52e5a917d0e12c1d55833a0b
Ahsan Ahmed Rakib <ahsan.rakib@technonext.com>
2024-10-30 11:24:10 +0600

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
@@ -7,11 +8,21 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // enable cors
   app.enableCors({
     origin: 'http://localhost:5000', // Allow requests only from this origin
-    methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
+    methods: 'GET,POST,PUT,PATCH,DELETE', // Allowed HTTP methods
     credentials: true, // Allow cookies to be sent
   });
+
+  //swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API documentation for my Nest.js application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(5000);
 }
